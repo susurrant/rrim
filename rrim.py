78fa91d521582220db984f5b3ef6c71db32dc623
Xin Yao <susurrant@outlook.com>
2017-11-24 17:21:25 +0800

    update description

--- rrim.py
+++ rrim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 """
- Implemented by Xin Yao : https://github.com/susurrant/'
+ Implemented by Xin Yao : https://github.com/susurrant/
 """
 
 import cv2
